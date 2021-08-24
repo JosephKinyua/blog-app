2824a12e0071ed23d2898b51e8bf334d27e49544
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-08-25 00:00:22 +0300

    initialization of the app

--- manage.py
+++ manage.py
@@ -4,3 +4,8 @@ from app.models import Blog, User, Comment
 from flask_migrate import Migrate, MigrateCommand
 
 app = create_app('development')
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('server', Server)
+manager.add_command('db', MigrateCommand)
