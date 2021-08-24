5eedcad3f7b4b13bfa2a29223a89ea73b1704fa0
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-08-25 00:06:17 +0300

    add a class prodconfig

--- config.py
+++ config.py
@@ -4,13 +4,23 @@ import secrets
 secret =secrets.token_urlsafe(32)
 
 class Config:
- SECRET_KEY = 'secret'
- MAIL_SERVER = 'smtp.googlemail.com'
- MAIL_PORT = 587
- MAIL_USE_TLS = True
- MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
- MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
- UPLOADED_PHOTOS_DEST = 'app/static/photos'
- QUOTE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
+  SECRET_KEY = 'secret'
+  MAIL_SERVER = 'smtp.googlemail.com'
+  MAIL_PORT = 587
+  MAIL_USE_TLS = True
+  MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+  MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+  UPLOADED_PHOTOS_DEST = 'app/static/photos'
+  QUOTE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
 
- 
+class ProdConfig(Config):
+   SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL','')
+  
+   if SQLALCHEMY_DATABASE_URI.startswith('postgres://'):
+       SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace('postgres://', "postgresql://", 1)
+
+
+
+
+
+
