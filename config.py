08eb32dcdf48cefa43244404053b0926255cf059
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-08-25 00:04:55 +0300

    add a class Config

--- config.py
+++ config.py
@@ -2,3 +2,15 @@ import os
 import secrets
 
 secret =secrets.token_urlsafe(32)
+
+class Config:
+ SECRET_KEY = 'secret'
+ MAIL_SERVER = 'smtp.googlemail.com'
+ MAIL_PORT = 587
+ MAIL_USE_TLS = True
+ MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+ MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+ UPLOADED_PHOTOS_DEST = 'app/static/photos'
+ QUOTE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
+
+ 
