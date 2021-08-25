28223c014b95f9057fc54a0edcab30d53cec864d
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-08-25 08:11:47 +0300

    login form validation

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -2,3 +2,9 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, ValidationError, BooleanField
 from wtforms.validators import Required, Email, EqualTo
 from ..models import User
+
+class LoginForm(FlaskForm):
+  email = StringField('Email Address', validators=[Required(), Email()])
+  password = PasswordField('Password', validators=[Required()])
+  remember = BooleanField('Remember me')
+  submit = SubmitField('Sign In')
