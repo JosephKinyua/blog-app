2ae9d211563c0fbe63d238100dfbfed64522c52e
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-08-25 21:01:43 +0300

    your descriptive commit message

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ from flask_script import Manager, Server
 from app.models import Blog, User, Comment
 from flask_migrate import Migrate, MigrateCommand
 
-app = create_app('development')
+app = create_app('production')
 
 migrate = Migrate(app, db)
 manager = Manager(app)
